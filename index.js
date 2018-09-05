6b87505bd908747a0c7724f6ff2ebdcc46ace8f3
Seb Gertz <sebgertz@gmail.com>
2018-09-05 21:58:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ function inOrder(currentNode){
   }
 }
 
+function findOrAdd(rootNode, newNode) {
+  let currentNode = rootNode;
+
+  if (newNode < currentNode) {
+    if (currentNode.left) {
+      return findOrAdd(currentNode, newNode)
+    } else {
+      currentNode.left = newNode
+    }
+  } else if (newNode < currentNode) {
+      if (currentNode.right) {
+        return findOrAdd(currentNode, newNode)
+      } else {
+        currentNode.right = newNode;
+      } 
+  }
+  
+}
